a842c0253bb599d6ded7372e38a1de412b9bfd2f
Aitor Zaldua <aitor.zaldua@gmail.com>
2021-10-18 09:53:25 +0200

    oct 18 1

--- tragedy_commons/tragedy-commons/dna/zomes/zome_01/src/player_profile.rs
+++ tragedy_commons/tragedy-commons/dna/zomes/zome_01/src/player_profile.rs
@@ -4,18 +4,19 @@ use crate::game_code::get_game_code_anchor;
 pub const PLAYER_LINK_TAG: &str = "PLAYER";
 
 //Holochain provee con hdk_entry la public_key y la hace publica
-#[hdk_entry(id = "player_profile", visibility = "public")]
-#[derive(Clone)]
 //player_id lo crea el backend y es la public key del agente dentro de la app
 //esta en la sourcechain
 //nickname lo introduce el usuario desde la UI
+#[hdk_entry(id = "player_profile", visibility = "public")]
+#[derive(Clone)]
 pub struct  PlayerProfile {
     pub player_id: AgentPubKey,
     pub nickname: String,
 }
 
 //wrapper de la info de la partida para fn join_game_with_code
-//¿¿¿¿?????
+//hay que traerse todas las opciones que queramos que haga el struct
+#[derive(Clone, Debug, Serialize, Deserialize, SerializedBytes)]
 pub struct JoinGameInfo {
     pub gamecode: String,
     pub nickname: String,
@@ -27,7 +28,11 @@ pub fn create_and_hash_entry_player_profile(nickname: String) -> ExternResult<En
     //recupera los datos del agente
     //todos los agentes son diferentes
     let agent = agent_info()?;
-
+    debug!(
+        "create_and_hash_entry_player_profile | nickname: {}, agent {:?}",
+        nickname,
+        agent.clone()
+    );
     //El struct sirve para crear el perfil en rust pero necesitamos
     //almacenar el profile en el DHT
     let player_profile = PlayerProfile {
@@ -38,17 +43,25 @@ pub fn create_and_hash_entry_player_profile(nickname: String) -> ExternResult<En
     //create_entry escribe el player profile en la DHT
     create_entry(&player_profile)?;
     //hash_entry crea el hash y lo devuelve como resultado de la funcion
-    //hash_entry NO escribe nada en el DHT 
+    //hash_entry NO escribe nada en el DHT
     hash_entry(&player_profile)
 }
 
 //unirse a la partida
 pub fn join_game_with_code (input: JoinGameInfo) -> ExternResult<EntryHash> {
+    info!("join_game_with_code | input: {:?}", input);
     //lo primero es el anchor, exista o no exista (partida nueva)
     //llamada a get_game_code_anchor luego arriba use crate::game_code::get_game_code_anchor;
-    let anchor = get_game_code_anchor(input.game_code)?;
+    let anchor = get_game_code_anchor(input.gamecode)?;
+    debug!("join_game_with_code | anchor created {:?}", &anchor);
+    // Create player's profile. So far it isn't connected to anything,
+    // just a combination of nickname & pub key
     //Crear el user profile para el input recibido
-    let player_profile_entry_hash = create_and_hash_entry_player_profile(input.nickname);
+    let player_profile_entry_hash = create_and_hash_entry_player_profile(input.nickname)?;
+    debug!(
+        "join_game_with_code | profile entry hash {:?}",
+        &player_profile_entry_hash
+    );
     //crear el link entre el anchor y el player_profile_entry_hash
     //create_link esta en la hdk
     //1er param: link base, donde empieza.
@@ -60,6 +73,7 @@ pub fn join_game_with_code (input: JoinGameInfo) -> ExternResult<EntryHash> {
         player_profile_entry_hash.into(),
         LinkTag::new(String::from(PLAYER_LINK_TAG)),
     )?;
+    debug!("join_game_with_code | link created");
     Ok(anchor)
 }
 
@@ -69,11 +83,13 @@ pub fn get_player_profile_for_game_code(
     short_unique_code: String,
 ) -> ExternResult<Vec<PlayerProfile>> {
     let anchor = get_game_code_anchor(short_unique_code)?;
+    debug!("anchor: {:?}", anchor);
     //get_links es del hdk
     //reupera los links con tag PLAYER_LINK_TAG
     //si se quiere TODOS los links, el segundo parametro peude ser None:
     //get_links(anchor,None)
     let links: Links = get_links(anchor, Some(LinkTag::new(String::from(PLAYER_LINK_TAG ))))?;
+    debug!("links: {:?}", links);
     //Data tranformation para pasar los links al vector
     let mut players = vec![];
     for link in links.into_inner() {
@@ -81,7 +97,7 @@ pub fn get_player_profile_for_game_code(
         //Element es la combinacion de dataheader y dataentry
         //get es una funcion del hdk
         let element: Element = get(link.target, GetOptions::default())?
-            .ok_or(WasmError::Guest(String__from("Entry nor found")))?;
+            .ok_or(WasmError::Guest(String::from("Entry nor found")))?;
         let entry_option = element.entry().to_app_option()?;
         let entry: PlayerProfile = entry_option.ok_or(WasmError::Guest(
             "The targeted entry is not agent pubkey".into(),
