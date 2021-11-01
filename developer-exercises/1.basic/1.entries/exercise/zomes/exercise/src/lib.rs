c7e74c8e681ded0a61725a14bcda6ce6d99b67bd
Aitor Zaldua <aitor.zaldua@gmail.com>
2021-11-01 17:30:19 +0100

    nov 1 1

--- developer-exercises/1.basic/1.entries/exercise/zomes/exercise/src/lib.rs
+++ developer-exercises/1.basic/1.entries/exercise/zomes/exercise/src/lib.rs
@@ -1,9 +1,25 @@
+use hdk::prelude::*;
 
+//Entry_defs
+entry_defs![
+ Greetings::entry_def()
+];
+
+//Structs
 #[derive(Serialize, Deserialize, Clone, Debug)]
 pub struct SomeExternalInput {
     content: String,
 }
 
-pub fn say_greeting(input: SomeExternalInput) -> ExternResult<HeaderHash> {
-    unimplemented!()
+#[hdk_entry(id = "greetings")]
+pub struct Greetings(String);
+
+
+//Functions
+#[hdk_extern]
+pub fn say_greeting(external_input: SomeExternalInput) -> ExternResult<HeaderHash> {
+
+    let output: Greetings = Greetings(external_input.content);
+
+    create_entry(output)
 }
